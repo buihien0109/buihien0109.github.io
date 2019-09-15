8de1eb2248f01b44c3891d3fb25110149625deb0
buihien0109 <buihien01091997@gmail.com>
2019-09-16 02:45:26 +0700

    update

---
+++ Carpo/js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+	var navbar = $('.container-fluid-nav');
+	
+	$(window).scroll(function(){
+		if($(window).scrollTop() <= 40){
+			navbar.removeClass('navbar-scroll');
+		} else {
+			navbar.addClass('navbar-scroll');
+		}
+	});
+});
