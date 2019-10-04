84aafba333bb5b0884e3bbac1fbb93aa34da6747
buihien0109 <buihien01091997@gmail.com>
2019-10-05 00:59:44 +0700

    "sua co chu"

--- Carpo/js/main.js
+++ Carpo/js/main.js
@@ -12,12 +12,25 @@ jQuery("#jtv-brand-logo-slider .slider-items").owlCarousel({
 });
 
 jQuery("#trending-slider .slider-items").owlCarousel({
-	items: 3, //10 items above 1000px browser width
-	itemsDesktop: [1024, 3], //5 items between 1024px and 901px
-	itemsDesktopSmall: [900, 2], // 3 items betweem 900px and 601px
+	items: 5, //10 items above 1000px browser width
+	itemsDesktop: [1024, 4], //5 items between 1024px and 901px
+	itemsDesktopSmall: [900, 3], // 3 items betweem 900px and 601px
+	itemsTablet: [640, 2], //2 items between 600 and 0;
+	itemsMobile: [360, 2],
+	navigation: true,
+	navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+	slideSpeed: 500,
+	pagination: false
+});
+
+jQuery("#latest-slider .slider-items").owlCarousel({
+	autoPlay: true,
+	items: 4, //10 items above 1000px browser width
+	itemsDesktop: [1024, 4], //5 items between 1024px and 901px
+	itemsDesktopSmall: [900, 3], // 3 items betweem 900px and 601px
 	itemsTablet: [640, 2], //2 items between 600 and 0;
 	itemsMobile: [360, 1],
-	navigation: true,
+	navigation: false,
 	navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
 	slideSpeed: 500,
 	pagination: false
