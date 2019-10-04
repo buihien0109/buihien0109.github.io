25acb83683b48ec7a7c781a6aa6080c58bac220b
buihien0109 <buihien01091997@gmail.com>
2019-10-04 21:07:13 +0700

    "update shop-list"

--- Carpo/js/main.js
+++ Carpo/js/main.js
@@ -1,11 +1,37 @@
-$(function(){
-	var navbar = $('.container-fluid-nav');
-	
-	$(window).scroll(function(){
-		if($(window).scrollTop() <= 40){
-			navbar.removeClass('navbar-scroll');
-		} else {
-			navbar.addClass('navbar-scroll');
-		}
-	});
+jQuery("#jtv-brand-logo-slider .slider-items").owlCarousel({
+	autoPlay: true,
+	items: 5, //10 items above 1000px browser width
+	itemsDesktop: [1024, 4], //5 items between 1024px and 901px
+	itemsDesktopSmall: [900, 3], // 3 items betweem 900px and 601px
+	itemsTablet: [640, 2], //2 items between 600 and 0;
+	itemsMobile: [360, 1],
+	navigation: false,
+	navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+	slideSpeed: 500,
+	pagination: false
+});
+
+jQuery("#trending-slider .slider-items").owlCarousel({
+	items: 3, //10 items above 1000px browser width
+	itemsDesktop: [1024, 3], //5 items between 1024px and 901px
+	itemsDesktopSmall: [900, 2], // 3 items betweem 900px and 601px
+	itemsTablet: [640, 2], //2 items between 600 and 0;
+	itemsMobile: [360, 1],
+	navigation: true,
+	navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+	slideSpeed: 500,
+	pagination: false
+});
+
+jQuery("#category-desc-slider .slider-items").owlCarousel({
+	autoplay: true,
+	items: 1, //10 items above 1000px browser width
+	itemsDesktop: [1024, 1], //5 items between 1024px and 901px
+	itemsDesktopSmall: [900, 1], // 3 items betweem 900px and 601px
+	itemsTablet: [600, 1], //2 items between 600 and 0;
+	itemsMobile: [360, 1],
+	navigation: true,
+	navigationText: ["<a class=\"flex-prev\"></a>", "<a class=\"flex-next\"></a>"],
+	slideSpeed: 500,
+	pagination: false
 });
