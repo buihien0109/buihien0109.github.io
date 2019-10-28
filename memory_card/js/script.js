6068754d8359d296b5ff742ddb906b1ae7115268
buihien0109 <buihien01091997@gmail.com>
2019-10-28 19:32:25 +0700

    update

--- memory_card/js/script.js
+++ memory_card/js/script.js
@@ -1,20 +1,20 @@
-var cards = [
-'images/card1.jpg',
-'images/card2.jpg',
-'images/card3.jpg',
-'images/card4.jpg',
-'images/card5.jpg'
+//Khai báo mảng card
+let cards = [
+	'images/card1.jpg',
+	'images/card2.jpg',
+	'images/card3.jpg',
+	'images/card4.jpg',
+	'images/card5.jpg'
 ];
 
-var count_card = 0;
+let count_card = 0;
+let current = null; //Card hiện tại
+let time = 30; //Khai báo thời gian đếm ngược = 30s
+let count = 0;  //Đếm số cặp bài trùng nhau
+let run = null; //Biến Interval
 
-var current = null;
-var time = 30;
-var count = 0;
-var run = null;
-
-window.onload = function() {
-	var imgArr = makeImgArray();
+window.onload = function () {
+	let imgArr = makeImgArray();
 	shuffle(imgArr);
 	drawImg(imgArr);
 	$('#timer').attr('max', time);
@@ -25,34 +25,32 @@ window.onload = function() {
 	$('.lose_dialog').hide();
 }
 
+//Nhân đôi mảng card
 function makeImgArray() {
-	var imgArr = [];
-	for (var i=0; i<cards.length; i++) {
-		imgArr.push(cards[i]);
-		imgArr.push(cards[i]);
-	}
+	let imgArr = [...cards, ...cards]; //Use ES6
 	return imgArr;
 }
 
+//Render mảng card ra ngoài view
 function drawImg(imgArr) {
-	for (var i=0; i<imgArr.length; i++) {
-		$('.wrapper').append('<div class="thumb">'+
-			'<div class="card" onclick="flipCard(this)" data-name="'+imgArr[i]+'">'+
-			'<div class="front">'+
-			'<img src="'+imgArr[i]+'" alt="card front">'+
-			'</div>'+
-			'<div class="back">'+
-			'<img src="images/back.jpg" alt="card back">'+
-			'</div>'+
-			'</div>'+
+	for (let i = 0; i < imgArr.length; i++) {
+		$('.wrapper').append('<div class="thumb">' +
+			'<div class="card" onclick="flipCard(this)" data-name="' + imgArr[i] + '">' +
+			'<div class="front">' +
+			'<img src="' + imgArr[i] + '" alt="card front">' +
+			'</div>' +
+			'<div class="back">' +
+			'<img src="images/back.jpg" alt="card back">' +
+			'</div>' +
+			'</div>' +
 			'</div>');
 	}
 	$('.wrapper').append('<div class="clear"></div>');
 }
 
-
+//Đảo vị trí mảng Cards
 function shuffle(array) {
-	var currentIndex = array.length, temporaryValue, randomIndex;
+	let currentIndex = array.length, temporaryValue, randomIndex;
 	while (0 !== currentIndex) {
 		randomIndex = Math.floor(Math.random() * currentIndex);
 		currentIndex -= 1;
@@ -63,7 +61,9 @@ function shuffle(array) {
 	return array;
 }
 
+//Handle khi chọn Card
 function flipCard(card) {
+	console.log(card)
 	if (count_card == 2) return;
 	if (!$(card).hasClass('flipped')) {
 		$(card).toggleClass('flipped');
@@ -73,9 +73,9 @@ function flipCard(card) {
 		} else {
 			count_card++;
 			if ($(card).attr("data-name") == $(current).attr("data-name")) {
-				$(current).find('.front').css('box-shadow','0 0 10px 5px green');
+				$(current).find('.front').css('box-shadow', '0 0 10px 5px green');
 				$(card).find('.front').css('box-shadow', '0 0 10px 5px green');
-				setTimeout(function() {
+				setTimeout(function () {
 					document.getElementById('success-music').play();
 					$(current).css('opacity', '0');
 					$(card).css('opacity', '0');
@@ -85,46 +85,48 @@ function flipCard(card) {
 					if (count == cards.length && time != 0) {
 						clearInterval(run);
 						$('.congrat_dialog').show();
-						setTimeout(function() {
+						setTimeout(function () {
 							document.getElementById('victory-music').play();
 						}, 500);
 					}
 				}, 800);
 			}
 			else {
-				$(current).find('.front').css('box-shadow','0 0 10px 5px red');
+				$(current).find('.front').css('box-shadow', '0 0 10px 5px red');
 				$(card).find('.front').css('box-shadow', '0 0 10px 5px red');
-				setTimeout(function() {
+				setTimeout(function () {
 					document.getElementById('failed-music').play();
 					$(current).toggleClass('flipped');
 					$(card).toggleClass('flipped');
-					$(current).find('.front').css('box-shadow','0 0 10px 5px white');
+					$(current).find('.front').css('box-shadow', '0 0 10px 5px white');
 					$(card).find('.front').css('box-shadow', '0 0 10px 5px white');
 					current = null;
 					count_card = 0;
 				}, 800);
-			} 
-		} 
+			}
+		}
 	}
 }
 
+//Handle start game và end game
 function start() {
-	$('.dialog_start').hide();
+	$('.dialog_start').hide(); //Click start thì popup start ẩn
 	$('.card').css('pointer-events', 'auto');
-	run = setInterval(function() {
+	run = setInterval(function () { //Gọi hàm setInterval đếm thời gian ngược 30s
 		time--;
-		$('#timer').attr('value', time);
-		if (time == 0)  {
+		$('#timer').attr('value', time); //Gán giá trị vào thanh progress bar
+		if (time == 0) { //Nếu time = 0 thì clear Interval và cho hiện popup end game
 			$('.card').css('pointer-events', 'none');
 			clearInterval(run);
 			$('.lose_dialog').show();
-			setTimeout(function() {
+			setTimeout(function () { //Sau 1s cho chạy nhạc gameover
 				document.getElementById('gameover-music').play();
 			}, 1000);
 		}
 	}, 1000);
 }
 
+//Xử lý khi replay lại game
 function replay() {
 	window.location.href = "index.html";
 }
