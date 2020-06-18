ba407461fef63fe0ad43d8afddd59392b59e3537
buihien0109 <buihien01091997@gmail.com>
2020-06-18 23:16:23 +0700

    hien

---
+++ course-vue/chapter01/app/seed.js
@@ -0,0 +1,39 @@
+const submissions = [
+    {
+        id: 1,
+        title: 'Yellow Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: 16,
+        avatar: 'https://zicxa.com/hinh-anh/wp-content/uploads/2019/07/T%E1%BB%95ng-h%E1%BB%A3p-h%C3%ACnh-%E1%BA%A3nh-g%C3%A1i-xinh-d%E1%BB%85-th%C6%B0%C6%A1ng-cute-nh%E1%BA%A5t-6.jpg',
+        submissionImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/11/F1_yellow_flag.svg/800px-F1_yellow_flag.svg.png',
+    },
+    {
+        id: 2,
+        title: 'Red Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: 19,
+        avatar: 'https://i0.wp.com/danongonline.com.vn/wp-content/uploads/2018/02/anh-girl-xinh-9-1.jpg?fit=624%2C563&ssl=1',
+        submissionImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/25/Red.svg/1200px-Red.svg.png',
+    },
+    {
+        id: 3,
+        title: 'Blue Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: 18,
+        avatar: 'https://media.ngoisao.vn/news/2015/05/29/hot-girl-malaysia-xinh-dep-khien-cu-dan-mang-dien-dao-1-ngoisao.vn.jpg',
+        submissionImage: 'https://assets.wordpress.envato-static.com/uploads/2018/01/image1.png',
+    },
+    {
+        id: 4,
+        title: 'Green Pail',
+        description: 'On-demand sand castle construction expertise.',
+        url: '#',
+        votes: 17,
+        avatar: 'https://nguyendangtam.com/wp-content/uploads/2019/08/anh-gai-xinh-ky-le-hieu-1.jpg',
+        submissionImage: 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/F1_green_flag.svg/1200px-F1_green_flag.svg.png',
+    },
+
+]
