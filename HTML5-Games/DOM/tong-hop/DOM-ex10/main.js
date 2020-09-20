163025b6355a811a569616511389469e907090fe
buihien0109 <buihien01091997@gmail.com>
2020-09-21 00:39:43 +0700

    hien

---
+++ HTML5-Games/DOM/tong-hop/DOM-ex10/main.js
@@ -0,0 +1,11 @@
+// Yêu cầu: Sử dụng Javascript để thực hiện các công việc sau
+
+// 1. Thêm 3 job bổ sung vào phần 'job' của trang bằng cách sao chép 'job-card' ban đầu.
+
+// 2. Cập nhật tiêu đề của các job mới thành : JavaScript Developer, Java Developer, Python Developer
+
+// 3. Cập nhật Jobs listed thành tổng số công việc hiện có trong trang
+
+// 4. Nhập tên công việc vào ô tìm kiếm để lọc các công việc (lọc theo tên công việc). Những công việc được tìm thấy sẽ hiển thị ra, còn lại sẽ bị ẩn đi
+
+// 5. Bấm vào Nút "All jobs" để reset ô tìm kiếm và hiển thị ra tất cả các công việc
