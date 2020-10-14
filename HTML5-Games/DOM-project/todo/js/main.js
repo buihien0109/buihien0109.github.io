b39d9a8182a3a43be297bd2563fe9344af0b0e64
buihien0109 <buihien01091997@gmail.com>
2020-10-14 09:24:56 +0700

    hien

--- HTML5-Games/DOM-project/todo/js/main.js
+++ HTML5-Games/DOM-project/todo/js/main.js
@@ -1,11 +1,9 @@
-let todo_list = document.querySelector(".todo-list");
-let input = document.getElementById("todo-input");
-let btn_add = document.getElementById("btn-add");
-let todo_option_item = document.querySelectorAll(".todo-option-item input");
-let todo_checkbox_item = document.querySelectorAll(".todo-item-title input");
+const todo_list = document.querySelector(".todo-list");
+const todo_input = document.querySelector("#todo-input");
+const btn_add = document.querySelector("#btn-add");
 
-let isUpdate = false; // Kiểm tra xem khi nào công việc update
-let idUpdate = ""; // ID của công việc đang thực hiện update là gì?
+let idUpdate = "";
+let isUpdate = false;
 
 // Tự động render ID của công việc ngẫu nhiên không trùng nhau
 function create_UUID() {
@@ -39,88 +37,63 @@ let todos = [{
     },
 ];
 
-btn_add.addEventListener("click", function () {
-    let todo = input.value;
-    if (todo == "") {
-        toastr.warning("Công việc không được để trống!");
-        return;
+// Thêm công việc
+btn_add.addEventListener('click', function() {
+    
+    let todoTitle = todo_input.value;
+    if(todoTitle == "") {
+        alert("Nội dung không được để trống");
+        return
     }
-    if (isUpdate) {
-        // Update công việc
+
+    if(isUpdate) {
+        // update
         for (let i = 0; i < todos.length; i++) {
             if (todos[i].id == idUpdate) {
-                todos[i].title = todo;
+                todos[i].title = todoTitle;
             }
         }
 
         btn_add.innerText = "Thêm";
         isUpdate = false;
         idUpdate = "";
+
     } else {
-        // Thêm công viêc mới
         let newTodo = {
-            id: create_UUID(),
-            title: todo,
-            status: false,
-        };
+            id : create_UUID(),
+            title : todoTitle,
+            status : false
+        }
+    
         todos.push(newTodo);
     }
 
-    input.value = "";
     renderUI(todos);
-});
+    todo_input.value = "";
+})
 
+// Render Todo
 function renderUI(arr) {
     todo_list.innerHTML = "";
 
-    // Kiểm tra nếu người dùng lựa chọn
-    // 1 - Xem tất cả các công việc
-    // 2 - Những công việc chưa hoàn thành
-    // 3 - Những công việc đã hoàn thành
-    let optionSelected = getOptionSelected();
-    let newTodos;
-    switch (optionSelected) {
-        case 1:
-            newTodos = [...arr];
-            break;
-        case 2:
-            newTodos = arr.filter((todo) => todo.status === false);
-            break;
-        case 3:
-            newTodos = arr.filter((todo) => todo.status === true);
-            break;
-        default:
-            newTodos = [...arr];
-            break;
-    }
+    if(arr.length == 0) {
+        todo_list.innerHTML = '<p class="todos-empty">Không có công việc nào trong danh sách</p>';
+        return
+    } 
 
-    // Kiểm tra khi trong danh sách không còn công việc nào cả
-    if (newTodos.length == 0) {
-        todo_list.innerHTML = "Không có công việc";
-        return;
-    }
-
-    // Duyệt danh sách công việc cần làm và render ra ngoài màn hình
-    for (let i = 0; i < newTodos.length; i++) {
+    for (let i = 0; i < arr.length; i++) {
+        const t = arr[i];
         todo_list.innerHTML += `
-            <div class="todo-item ${newTodos[i].status ? "active-todo" : ""}">
+            <div class="todo-item ${t.status ? `active-todo` : ``}">
                 <div class="todo-item-title">
-                    ${
-                      newTodos[i].status
-                        ? `<input type="checkbox" checked onclick="toggleStatusTodo('${newTodos[i].id}')"></input>`
-                        : `<input type="checkbox" onclick="toggleStatusTodo('${newTodos[i].id}')"></input>`
-                    }
-                    <p>${newTodos[i].title}</p>
+                    <input type="checkbox" ${t.status ? `checked` : ``} onclick="toggleStatus('${t.id}')">
+                    <p>${t.title}</p>
                 </div>
                 <div class="option">
-                    <button class="btn btn-update" onclick="updateTodo('${
-                      newTodos[i].id
-                    }')">
+                    <button class="btn btn-update" onclick="updateTodo('${t.id}')">
                         <img src="./img/pencil.svg" alt="icon">
                     </button>
-                    <button class="btn btn-delete" onclick="deleteTodo('${
-                      newTodos[i].id
-                    }')">
+                    <button class="btn btn-delete" onclick="deleteTodo('${t.id}')">
                         <img src="./img/remove.svg" alt="icon">
                     </button>
                 </div>
@@ -129,24 +102,7 @@ function renderUI(arr) {
     }
 }
 
-// Kiểm tra khi có sự thay đổi trong nút xem công việc thì render lại UI
-todo_option_item.forEach((btn) => {
-    btn.addEventListener("change", function () {
-        renderUI(todos);
-    });
-});
-
-// Lấy giá trị trong 1 ô radio
-function getOptionSelected() {
-    for (let i = 0; i < todo_option_item.length; i++) {
-        if (todo_option_item[i].checked) {
-            return parseInt(todo_option_item[i].value);
-        }
-    }
-}
-
-// Thay đổi status của 1 công việc thì render lại UI
-function toggleStatusTodo(id) {
+function toggleStatus(id) {
     for (let i = 0; i < todos.length; i++) {
         if (todos[i].id == id) {
             todos[i].status = !todos[i].status;
@@ -155,48 +111,34 @@ function toggleStatusTodo(id) {
     renderUI(todos);
 }
 
-// Xóa công việc thì render lại UI
 function deleteTodo(id) {
     for (let i = 0; i < todos.length; i++) {
         if (todos[i].id == id) {
-            todos.splice(i, 1);
+            todos.splice(i,1)
         }
     }
     renderUI(todos);
 }
 
-// Update công việc thì render lại UI
 function updateTodo(id) {
-    let title = "";
+    let title
     for (let i = 0; i < todos.length; i++) {
         if (todos[i].id == id) {
             title = todos[i].title;
         }
     }
 
-    btn_add.innerText = "Cập nhật";
-    input.value = title;
-    input.focus();
+    btn_add.innerText = "CẬP NHẬT";
 
-    isUpdate = true;
+    todo_input.value = title;
+    todo_input.focus();
+
     idUpdate = id;
+    isUpdate = true;
 }
 
 function init() {
     renderUI(todos);
 }
 
-function addToLocalStorage(todos) {
-    localStorage.setItem("todos", JSON.stringify(todos));
-    renderUI(todos);
-}
-
-function getFromLocalStorage() {
-    const reference = localStorage.getItem("todos");
-    if (reference) {
-        todos = JSON.parse(reference);
-        renderUI(todos);
-    }
-}
-
-window.onload = init();
+window.onload = init;
