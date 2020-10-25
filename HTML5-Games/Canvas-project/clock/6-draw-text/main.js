f2b4bc6553aaf7f4bac39c87631c0034d73cd45d
buihien0109 <buihien01091997@gmail.com>
2020-10-25 23:45:58 +0700

    hien

---
+++ HTML5-Games/Canvas-project/clock/6-draw-text/main.js
@@ -0,0 +1,180 @@
+const canvas = document.getElementById("canvas");
+const c = canvas.getContext("2d");
+
+canvas.width = innerWidth;
+canvas.height = innerHeight;
+
+
+let radius = canvas.height / 5;
+let centerX = canvas.width / 2;
+let centerY = canvas.height / 3;
+
+// Khởi tạo class Hình tròn
+class Arc {
+    constructor(x, y, radius, color) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+        this.color = color;
+    }
+
+    draw() {
+        c.beginPath();
+        c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+}
+
+// Khởi tạo class Hình chữ nhật
+class Rect {
+    constructor(x, y, width, height, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    draw() {
+        c.beginPath();
+        c.rect(this.x, this.y, this.width, this.height);
+        c.fillStyle = this.color;
+        c.fill();
+    }
+}
+
+class Clock {
+    constructor(x, y, radius) {
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+
+    drawFace() {
+        //Draw the outer frame
+        let rect = new Rect(this.x - this.radius, this.y - this.radius * 1.5, 2 * this.radius, 4 * this.radius, '#f0e5d0')
+        rect.draw();
+
+        //Draw the another frame
+        let rect1 = new Rect(this.x - this.radius * 0.5, this.y - this.radius * 1.6, this.radius, 4.8 * this.radius, '#937a62')
+        rect1.draw();
+
+        //Draw the frame
+        let arc = new Arc(this.x, this.y, this.radius * 1.2, '#cba370')
+        arc.draw();
+        
+        let arc1 = new Arc(this.x, this.y, this.radius, '#EBEAE4')
+        arc1.draw();
+        
+        let arc2 = new Arc(this.x, this.y, this.radius * 0.08, '#000')
+        arc2.draw();
+    }
+
+    drawDot() {
+        let radianPerDot = Math.PI / 30;
+        let ang;
+        let dot;
+        for (let i = 1; i < 61; i++) {
+            ang = i * radianPerDot;
+            if (i % 5 === 0) {
+                dot = new Arc(this.x + this.radius * 0.8 * Math.sin(ang), this.y - this.radius * 0.8 * Math.cos(ang), this.radius * 0.02, '#000');
+            } else {
+                dot = new Arc(this.x + this.radius * 0.8 * Math.sin(ang), this.y - this.radius * 0.8 * Math.cos(ang), this.radius * 0.005, '#000');
+            }
+            dot.draw();
+        }
+    }
+
+    drawNumber() {
+        let radianPerNumber = Math.PI / 6;
+
+        c.font = "24px Arial";
+        c.textBaseline = "middle";
+        c.textAlign = "center";
+
+        let ang;
+        for (let i = 1; i < 13; i++) {
+            ang = i * radianPerNumber;
+
+            c.beginPath();
+            c.fillStyle = '#000';
+            c.fillText(`${i}`, this.x + this.radius * 0.9 * Math.sin(ang), this.y - this.radius * 0.9 * Math.cos(ang));
+        }
+    }
+
+    drawHand(angle, length, width, color) {
+        c.beginPath();
+        c.lineWidth = width;
+        c.lineCap = "round";
+        c.strokeStyle = color;
+
+        c.moveTo(this.x, this.y);
+        c.lineTo(this.x + length * Math.sin(angle), this.y - length * Math.cos(angle));
+    
+        c.stroke();
+    }
+
+    drawTime() {
+        let now = new Date();
+
+        let hour = now.getHours();
+        let minute = now.getMinutes();
+        let second = now.getSeconds();
+        let milisecond = now.getMilliseconds();
+
+        hour = hour % 12;
+
+        let anglePerHour = (hour * Math.PI / 6);
+        let anglePerMinute = (minute * Math.PI / 30);
+        let anglePerSecond = (second * Math.PI / 30);
+        let anglePerMilisecond = (milisecond * Math.PI / 30000);
+
+        let angleHour = anglePerHour + anglePerMinute * 60 + anglePerSecond * 3600;
+        this.drawHand(angleHour, radius * 0.5, 15,'#000');
+
+        let angleMinute = anglePerMinute + anglePerSecond * 60;
+        this.drawHand(angleMinute, radius * 0.7, 10,'#000');
+
+        let angleSecond = anglePerSecond + anglePerMilisecond;
+        this.drawHand(angleSecond, radius * 0.8, 4,'#F7633A');
+    }
+
+    drawText() {
+        c.beginPath();
+        c.font = '20px Arial';
+        c.textAlign = 'center';
+        c.fillText(getDate(),this.x,this.y-this.radius*0.6);    
+    }
+    
+}
+
+let clock = new Clock(centerX, centerY, radius);
+
+function update() {
+    clock.drawFace();
+    clock.drawDot();
+    clock.drawNumber();
+    clock.drawTime();
+    clock.drawText();
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, canvas.width, canvas.height);
+    update();
+}
+animate();
+
+
+function getDate() {
+    var date = new Date();
+
+    let ampm = date.getHours() >= 12 ? ' PM' : ' AM';
+
+    let hour = ('0' + (date.getHours() % 12)).slice(-2);
+    let minute = ('0' + (date.getMinutes())).slice(-2);
+    let second = ('0' + (date.getSeconds())).slice(-2);
+    
+    return `${hour}:${minute}:${second} ${ampm}`;
+}
