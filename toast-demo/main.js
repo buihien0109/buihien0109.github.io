eb30efffe5f7005fea6e3a6c45d300217b46eef7
buihien0109 <buihien01091997@gmail.com>
2022-03-16 09:23:23 +0700

    first commit

---
+++ toast-demo/main.js
@@ -0,0 +1,22 @@
+toastr.options = {
+    "closeButton": true,
+    "debug": false,
+    "newestOnTop": false,
+    "progressBar": false,
+    "positionClass": "toast-top-center",
+    "preventDuplicates": false,
+    "onclick": null,
+    "showDuration": "300",
+    "hideDuration": "1000",
+    "timeOut": "5000",
+    "extendedTimeOut": "1000",
+    "showEasing": "swing",
+    "hideEasing": "linear",
+    "showMethod": "fadeIn",
+    "hideMethod": "fadeOut"
+}
+
+const btn = document.querySelector(".btn")
+btn.addEventListener("click", function() {
+    toastr.success('Have fun storming the castle!', 'Miracle Max Says')
+})
